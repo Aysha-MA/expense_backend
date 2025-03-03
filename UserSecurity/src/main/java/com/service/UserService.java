2a27a2fbdec06d953f0c59f0250558fdcd7c19d5
Aysha M A <aishmish2001@gmail.com>
2025-03-03 11:22:44 +0530

    initial

---
+++ UserSecurity/src/main/java/com/service/UserService.java
@@ -0,0 +1,31 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.entity.UserInfo;
+import com.repository.UserInfoRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserInfoRepository repository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public String addUser(UserInfo userInfo) {
+		String name = userInfo.getName();
+		UserInfo obj1 = repository.findByName(name).orElse(null);
+		System.out.println(obj1);
+		if (obj1 == null) {
+			userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+			repository.save(userInfo);
+			return "Registration Successfully ";
+		} else {
+			return "This User is Already Registered.";
+		}
+	}
+
+}
